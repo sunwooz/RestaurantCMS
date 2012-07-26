03a1e6bc32f70ee276c99d4de126928148cadfa8
Daniel Yang <yangsunwoo@gmail.com>
2012-07-25 22:23:50 -0400

    added attr_accessible :image_file_name

--- app/models/menuitem.rb
+++ app/models/menuitem.rb
@@ -1,6 +1,5 @@
 class Menuitem < ActiveRecord::Base
-  attr_accessible :name, :description, :price, :image
-  attr_accessor :image_file_name
+  attr_accessible :name, :description, :price, :image_file_name
   
   has_attached_file :image, :styles => { :small => "150x150>" }
 end
