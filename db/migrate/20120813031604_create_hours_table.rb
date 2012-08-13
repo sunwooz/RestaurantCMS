7a97444d6463c695ad909cad05c24c00330715fc
Daniel Yang <yangsunwoo@gmail.com>
2012-08-12 23:24:52 -0400

    add restaurant_id to hours

--- db/migrate/20120813031604_create_hours_table.rb
+++ db/migrate/20120813031604_create_hours_table.rb
@@ -1,20 +1,21 @@
 class CreateHoursTable < ActiveRecord::Migration
   def self.up
     create_table :hours do |t|
-      t.string :from_monday, :string
-      t.string :to_monday, :string
-      t.string :from_tuesday, :string
-      t.string :to_tuesday, :string
-      t.string :from_wednesday, :string
-      t.string :to_wednesday, :string
-      t.string :from_thursday, :string
-      t.string :to_thursday, :string
-      t.string :from_friday, :string
-      t.string :to_friday, :string
-      t.string :from_saturday, :string
-      t.string :to_saturday, :string
-      t.string :from_sunday, :string
-      t.string :to_sunday, :string
+      t.string :from_monday
+      t.string :to_monday
+      t.string :from_tuesday
+      t.string :to_tuesday
+      t.string :from_wednesday
+      t.string :to_wednesday
+      t.string :from_thursday
+      t.string :to_thursday
+      t.string :from_friday
+      t.string :to_friday
+      t.string :from_saturday
+      t.string :to_saturday
+      t.string :from_sunday
+      t.string :to_sunday
+      t.integer :restaurant_id
     end
   end
 
