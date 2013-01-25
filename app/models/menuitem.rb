d156f26b3f9a2626129478a1f57c702906c70ddc
Daniel Yang <yangsunwoo@gmail.com>
2013-01-24 19:42:59 -0500

    config menuitem model with s3 credential variables

--- app/models/menuitem.rb
+++ app/models/menuitem.rb
@@ -6,8 +6,16 @@ class Menuitem < ActiveRecord::Base
 
   belongs_to :restaurant
 
-  has_attached_file :image, :styles => {
-    :thumbnail => "100x101",
-    :regular => "560x568>"
-    }
+  has_attached_file :image,
+    :styles => {
+      :thumbnail => "100x101",
+      :regular => "560x568>"
+    },
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    },
+    :path => "menuimages/:filename"
 end
