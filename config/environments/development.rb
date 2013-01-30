9c7d5f064c7a96325df579af6a1381988af82e49
Daniel Yang <yangsunwoo@gmail.com>
2013-01-29 19:51:57 -0500

    slightly improve home design

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,13 +35,14 @@ Restauranttest::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
+  Paperclip.options[:command_path] = "/usr/local/bin/"
+
   config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_protocol => 'http',
-    :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+      :storage => :s3,
+      :s3_credentials => {
+        :bucket => ENV['AWS_BUCKET'],
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
   }
 end
